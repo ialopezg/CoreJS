cab013b4b0dc273008179fd5d8505ba66ef980c7
Isidro A. López G <me@ialopezg.com>
2022-11-16 12:36:50 -0500

    fix: improve isEmpty helper

--- src/common/helpers/is-empty.helper.ts
+++ src/common/helpers/is-empty.helper.ts
@@ -1,7 +1,10 @@
-import { isFunction, isUndefined } from '../utils';
-import { isNumber } from './is-number.helper';
-import { isDate } from './is-date.helper';
+import { isArray } from './is-array.helper';
 import { isBoolean } from './is-boolean.helper';
+import { isDate } from './is-date.helper';
+import { isFunction } from './is-function.helper';
+import { isNumber } from './is-number.helper';
+import { isString } from './is-string.helper';
+import { isUndefined } from './is-undefined.helper';
 
 /**
  * Whether given object value is empty.
@@ -16,23 +19,24 @@ export const isEmpty = (value: any): boolean => {
     return true;
   }
 
+  // null value
+  if (value == null) {
+    return true;
+  }
+
+  // function, number, boolean, or date
   if (isFunction(value) || isNumber(value) || isBoolean(value) || isDate(value)) {
     return false;
   }
 
-  // null or length array equal 0
-  if (value == null || value.length === 0) {
-    return true;
-  }
-
   // empty object
   if (typeof value === 'object') {
-    let result = true;
-    for (const v in value) {
-      result = false;
-    }
+    return Object.keys(value).length === 0;
+  }
 
-    return result;
+  // string or array
+  if (isString(value) || isArray(value)) {
+    return !value.length;
   }
 
   return false;
