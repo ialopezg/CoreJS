879b352b7daafc94076dd8e11f3b7b10d1f1391d
Isidro A. López G <me@ialopezg.com>
2022-11-16 12:36:12 -0500

    fix: clean code
    - add isConstructor helper file

---
+++ src/common/helpers/is-constructor.helper.ts
@@ -0,0 +1,23 @@
+import { isUndefined } from './is-undefined.helper';
+
+/**
+ * Validates if given object value is a constructor function.
+ *
+ * @param fn Object to be analyzed.
+ *
+ * @returns True if value is a constructor function, otherwise false.
+ */
+export const isConstructor = (fn: any): boolean => {
+  if (isUndefined(fn)) {
+    return false;
+  }
+
+  try {
+    // eslint-disable-next-line no-new
+    new new Proxy(fn, { construct() { return {}; } })();
+
+    return true;
+  } catch (error: any) {
+    return error.message.indexOf('is not a constructor') < 0;
+  }
+};
